cc34bcceb97dc767e75edb4f00b525ed589ed628
Lagadapati Sai Nagarjuna <lagadapati.nagarjuna2007@gmail.com>
2026-08-02 20:04:29 +0530

    Time: 2 ms (38.17%), Space: 8.7 MB (21.21%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long temp=x;
+        long long  rev=0;
+        while(temp!=0)
+        {
+            rev=rev*10+(temp%10);
+            temp/=10;
+        }
+        if (rev>INT_MAX||rev<INT_MIN)
+        {
+         return 0;
+        }
+        return rev;
+    }
+};
