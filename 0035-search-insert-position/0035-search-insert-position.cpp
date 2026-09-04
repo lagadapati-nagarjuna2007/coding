9f0adad235e49107b1c0e1a5e63e88b6cc06f313
Lagadapati Sai Nagarjuna <lagadapati.nagarjuna2007@gmail.com>
2026-09-04 22:42:20 +0530

    Time: 0 ms (100%), Space: 13.5 MB (78.01%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,23 +4,20 @@ public:
         int l=0,r=nums.size()-1;
         while(l<=r)
         {
-            if(nums[r]==target)
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target)
             {
-                return r;
+                return mid;
             }
-             else if(nums[r]<target)
+            else if(nums[mid]<target)
             {
-                return r+1;
-            }
-            else if(nums[r]>target)
-            {
-                r--;
+                l=mid+1;
             }
             else
             {
-                break;
+                r=mid-1;
             }
         }
-        return 0;
+        return l;
     }
 };
