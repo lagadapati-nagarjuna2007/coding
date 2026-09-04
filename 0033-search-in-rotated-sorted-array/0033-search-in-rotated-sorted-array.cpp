7eb87781d2f66fb5b10a80299b458a321eee6842
Lagadapati Sai Nagarjuna <lagadapati.nagarjuna2007@gmail.com>
2026-09-04 23:17:49 +0530

    Time: 0 ms (100%), Space: 15.1 MB (69.19%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l=0,r=nums.size()-1;
-        while(l<=r)
+       int l=0,r=nums.size()-1;
+       while(l<=r)
+       {
+        int mid=l+(r-l)/2;
+        if(nums[mid]==target)
         {
-            int mid=l+(r-l)/2;
-            if(nums[mid]==target)
-            {
             return mid;
-            }
-            if(nums[mid]>=nums[l])
-            {
-              if(nums[mid]>=target&& nums[l]<=target)
-              {
-                r=mid-1;
-              }
-            else
-            {
-                l=mid+1;
-            }
-            }
-            else
-            {
-            if(target>nums[mid]&&target<=nums[r])
-            {
-                l=mid+1;
-            }
-            else
-            {
-                r=mid-1;
-            }}
         }
-        return -1;
+        if(nums[mid]>=nums[l])
+        {
+        if(nums[mid]>target&& nums[l]<=target)
+        {
+            r=mid-1;
+        }
+        else
+        {
+            l=mid+1;
+        }
+       }
+       else
+       {
+        if(nums[mid]<target && nums[r]>=target)
+        {
+            l=mid+1;
+        }
+        else
+        {
+            r=mid-1;
+        }
+       }} 
+    return -1;
     }
 };
