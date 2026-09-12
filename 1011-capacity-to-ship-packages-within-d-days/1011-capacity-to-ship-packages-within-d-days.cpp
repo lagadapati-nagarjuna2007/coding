cc5c4519f4ddc1e480e22981adaaa9e51616d437
Lagadapati Sai Nagarjuna <lagadapati.nagarjuna2007@gmail.com>
2026-09-12 19:48:58 +0530

    Time: 11 ms (65.44%), Space: 34.9 MB (88.12%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-        int l=*max_element(weights.begin(),weights.end()),ans=0;
+        int l=*max_element(weights.begin(),weights.end());
         int r=accumulate(weights.begin(),weights.end(),0);
+        int ans=0;
         while(l<=r)
         {
             int mid=l+(r-l)/2;
-            if(check(weights,days,mid))
+            if(ischeck(weights,days,mid))
             {
-                ans=mid;
-                r=mid-1;
+            ans=mid;
+            r=mid-1;
             }
             else
             {
@@ -18,7 +19,7 @@ public:
         }
         return ans;
     }
-    bool check(vector<int>& weights, int days,int capacity)
+    int ischeck(vector<int>& weights, int days,int capacity)
     {
         int sum=0,day=1;
         for(auto p:weights)
@@ -28,8 +29,7 @@ public:
             {
                 day++;
                 sum=p;
-            }
-        }
-        return day<=days;
+            }}
+            return day<=days;
     }
 };
