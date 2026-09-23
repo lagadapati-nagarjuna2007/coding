982ac99edfb8d6d1bd7c3218a548bf0e5de1ca8e
Lagadapati Sai Nagarjuna <lagadapati.nagarjuna2007@gmail.com>
2026-09-23 21:54:27 +0530

    Time: 14 ms (87.62%), Space: 102.8 MB (79.21%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n=temperatures.size(),i,j=n-1;
+        vector<int>ans(n,0);
+        stack<int>s;
+        for(i=n-1;i>=0;i--)
+        {
+            while(!s.empty()&& temperatures[s.top()]<=temperatures[i])
+            {
+                s.pop();
+            }
+            if(!s.empty()&& temperatures[s.top()]>temperatures[i])
+            {
+                ans[i]=s.top()-i;
+            }
+            s.push(i);
+        }
+        return ans;
+    }
+};
